50db6bf0fc5c567fbde44e69edf0d02243c83a53
vassilismourikis <vassilismourikis@gmail.com>
2022-06-13 15:59:17 +0300

    trim

--- src/BrokerNode.java
+++ src/BrokerNode.java
@@ -134,7 +134,7 @@ public class BrokerNode{
                                 }
                     }
                     else if(input.startsWith("/getvideo")) {
-                        ArrayList<byte[]> chunkss = (new MultimediaValue(null,new MultimediaFile(input.substring(10),"server"))).getMultimediaFile().getMultimediaFileChunk();
+                        ArrayList<byte[]> chunkss = (new MultimediaValue(null,new MultimediaFile(input.substring(10).trim(),"server"))).getMultimediaFile().getMultimediaFileChunk();
                         for(byte[] chunk : chunkss) {
                             out.writeObject(chunk);
                             out.flush();
